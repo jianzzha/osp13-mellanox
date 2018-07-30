27a51b359d23b4e25a07e7ac78db831d9ff20a2d
jianzzha <jianzzha@redhat.com>
2018-07-30 15:05:24 -0400

    old file from osp12

---
+++ non_odl_deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+error ()
+{
+  echo $* 1>&2
+  exit 1
+}
+
+source /home/stack/stackrc || error "can't load stackrc"
+if openstack stack list | grep overcloud; then
+  openstack stack delete overcloud --wait --y
+fi
+
+clean_done=0
+for i in $(seq 600); do
+  sleep 3
+  if ironic node-list | grep clean; then
+    continue
+  else
+    clean_done=1
+    break
+  fi
+done
+
+# add -e $PWD/disable-telemetry.yaml to disable ceilometry
+# -e /home/stack/docker_registry.yaml
+openstack overcloud deploy \
+--templates \
+-r $PWD/roles_data.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
+-e /home/stack/docker_registry.yaml \
+-e $PWD/network-environment.yaml \
+-e $PWD/disable-telemetry.yaml \
+--ntp-server clock.redhat.com
+
+[ $? -eq 0 ] || exit 1
+echo "modify /etc/hosts entry"
+sudo sed -i -r '/compute/d' /etc/hosts
+sudo sed -i -r '/controller/d' /etc/hosts
+nova list | sed -r -n 's/.*(compute-[0-9]+).*ctlplane=([0-9.]+).*/\2 \1/p' | sudo tee --append /etc/hosts >/dev/null
+nova list | sed -r -n 's/.*(controller-[0-9]+).*ctlplane=([0-9.]+).*/\2 \1/p' | sudo tee --append /etc/hosts >/dev/null
+
